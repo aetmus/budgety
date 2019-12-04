58915fc9fd61dd023ea7fad87802ad5e47864a78
Andrew Etmus <andrew.etmus94@gmail.com>
2019-12-03 23:39:25 -0600

    stuck. thinking some sort of plugin will help

--- drag.js
+++ drag.js
@@ -1,16 +1,15 @@
 function dragstart_handler(e) {
-  e.dataTransfer.setData('text/html', e.target.innerHTML);
-  e.dataTransfer.dropEffect = 'move';
+    e.dataTransfer.setData('text/html', e.target.innerHTML);
+    e.dataTransfer.dropEffect = 'copy';
 }
 
 function dragover_handler(e) {
-  e.preventDefault();
-  e.dataTransfer.dropEffect = 'move';
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'move';
 }
 
 function drop_handler(e) {
-  e.preventDefault();
-  let data = e.dataTransfer.getData('text/html');
-  e.target.innerHTML = data;
-  e.dataTransfer.dropEffect = "move";
+    e.preventDefault();
+    var data = e.dataTransfer.getData('text/html');
+    e.target.appendChild(document.getElementById(data));
 }
